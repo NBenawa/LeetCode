9d8b264b4e86bdbcf6920a5c7e1767a201dfbd97
Najibullah Mohammadi <najeeb.benawa@gmail.com>
2026-06-04 11:19:41 +0200

    removing extra checkings

--- PascalsTriangle.java
+++ PascalsTriangle.java
@@ -24,7 +24,7 @@ public class PascalsTriangle {
         for (int i = 0; i < numRows; i++) {
             List<Integer> row = new ArrayList<>();
             for (int j = 0; j <= i; j++) {
-                if (i == 0 || i == 1 || j == 0 || j == i) {
+                if (j == 0 || j == i) {
                     row.add(1);
                 } else {
                     row.add(triangle.get(i - 1).get(j - 1) + triangle.get(i - 1).get(j));
