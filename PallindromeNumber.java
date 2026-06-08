2f4ff32ab167bad29f8677b612afea62eabffe6f
Najibullah Mohammadi <najeeb.benawa@gmail.com>
2026-06-08 09:16:16 +0200

    adding a second solution

--- PallindromeNumber.java
+++ PallindromeNumber.java
@@ -3,24 +3,39 @@ import java.util.List;
 
 public class PallindromeNumber {
     public static void main(String[] args) {
-        int num = 121;
+        int num = 122;
         System.out.println(isPalindrome(num));
     }
+    // private static boolean isPalindrome(int x) {
+    //     List<Integer> digits = new ArrayList<>();
+    //     while (x > 0) {
+    //         digits.add(x % 10);
+    //         x /= 10;
+    //     }
+    //     int left = 0;
+    //     int right = digits.size() - 1;
+    //     while (left < right) {
+    //         if (digits.get(left) != digits.get(right)) {
+    //             return false;
+    //         }
+    //         left++;
+    //         right--;
+    //     }
+    //     return true;
+    // }
     private static boolean isPalindrome(int x) {
-        List<Integer> digits = new ArrayList<>();
-        while (x > 0) {
-            digits.add(x % 10);
+        if (x < 0) {
+            return false;
+        }
+        if (x == 0) {
+            return true;
+        }
+        int reversed = 0;
+        while (x > reversed) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
             x /= 10;
         }
-        int left = 0;
-        int right = digits.size() - 1;
-        while (left < right) {
-            if (digits.get(left) != digits.get(right)) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-        return true;
+        return (x == reversed || x == reversed / 10);
     }
 }
