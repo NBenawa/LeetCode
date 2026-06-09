56c81d91de07d9e9a88fad3fcdfc778b2f069de3
Najibullah Mohammadi <najeeb.benawa@gmail.com>
2026-06-09 20:31:33 +0200

    completing Vallind pallindrom problem

---
+++ ValidPallindrom.java
@@ -0,0 +1,25 @@
+public class ValidPallindrom {
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+        boolean result = isPalindrome(s);
+        System.out.println(result); // Output: true
+    }
+    private static boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
