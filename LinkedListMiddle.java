e707b9512e65864fa9d747af5179a9879c90f5db
Najibullah Mohammadi <najeeb.benawa@gmail.com>
2026-06-09 20:32:48 +0200

    completing linked list middle

---
+++ LinkedListMiddle.java
@@ -0,0 +1,26 @@
+public class LinkedListMiddle {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        ListNode middle = findMiddle(head);
+        System.out.println(middle.val); // Output: 3
+    }
+
+    private static ListNode findMiddle(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    
+}
