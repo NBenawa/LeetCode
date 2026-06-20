8c4ea22c6f888b8b825c46584b6952e186064d4f
Najibullah Mohammadi <najeeb.benawa@gmail.com>
2026-06-20 21:41:14 +0200

    completing the find left tree node problem

--- FindLeftTree.java
+++ FindLeftTree.java
@@ -1,24 +1,29 @@
-import java.util.HashMap;
-
 public class FindLeftTree {
+    static int level = -1;
+    static int result = 0;
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1);
         root.left = new TreeNode(2);
-        root.right = new TreeNode(2);
-        root.left.left = new TreeNode(3);
-        root.left.right = new TreeNode(4);
-        root.right.left = new TreeNode(4);
-        root.right.right = new TreeNode(3);
-        System.out.println(findLeftTree(new HashMap(), root));
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.right.left = new TreeNode(5);
+        root.right.right = new TreeNode(6);
+        root.right.left.left = new TreeNode(7);
+        root.right.left.left.left = new TreeNode(9);
+        findLeftTree(0, root);
+        System.out.println(result);
     }
 
-    private static TreeNode findLeftTree(HashMap hashMap, TreeNode root) {
-        if (root.left != null) {
-            findLeftTree(hashMap, root.left);
-        } else {
-            return root;
+    private static void findLeftTree(int currLevel, TreeNode root) {
+        if (root == null) {
+            return;
         }
-        return null;
+        if (currLevel > level) {
+            level = currLevel;
+            result = root.val;
+        }
+        findLeftTree(currLevel+1, root.left);
+        findLeftTree(currLevel+1, root.right);
     }
 }
 
