7d600b41af6b0ee81aed3df8f2da42b1382cf8dc
Najibullah Mohammadi <najeeb.benawa@gmail.com>
2026-07-10 09:21:12 +0200

    completing max consecutive ones problem

---
+++ MaxConsecutiveOnes.java
@@ -0,0 +1,21 @@
+public class MaxConsecutiveOnes {
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 0, 1, 1, 1};
+        int maxConsecutiveOnes = findMaxConsecutiveOnes(nums);
+        System.out.println("Max consecutive ones: " + maxConsecutiveOnes);
+    }
+
+    private static int findMaxConsecutiveOnes(int[] nums) {
+        int maxCount = 0;
+        int currentCount = 0;
+        for (int num : nums) {
+            if (num == 1) {
+                currentCount++;
+                maxCount = maxCount >= currentCount ? maxCount : currentCount;
+            } else {
+                currentCount = 0;
+            }
+        }
+        return maxCount;
+    }
+}
